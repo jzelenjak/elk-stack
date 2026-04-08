416143ec6a25a12bb984fd4fe0d301142edbf3d0
Jegor Zelenjak <jzelenjak@proton.me>
2026-04-08 12:25:30 +0300

    Update Logstash configuration

--- start_logstash.sh
+++ start_logstash.sh
@@ -6,20 +6,23 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+NETWORK="elk-net"
+PUBCERTS_VOLUME="elk-pubcerts"
+LOGSTASHDATA_VOLUME="logstashdata02"
 
 LOGSTASH_DIR="/usr/share/logstash"
 LOGSTASH_PIPELINE_DIR="${LOGSTASH_DIR}/pipeline"
 LOGSTASH_CONF_DIR="${LOGSTASH_DIR}/config"
 LOGSTASH_CERT_DIR="${LOGSTASH_CONF_DIR}/certs"
-LOGSTASH_CONF="logstash.conf"
 
 [ -f .env ] && source .env || { echo "$0: cannot find .env file" >&2; exit 1; }
 
-docker run --rm --name "logstash02" --network "elk-net" \
-    -v "$(pwd)/${LOGSTASH_CONF}:${LOGSTASH_PIPELINE_DIR}/${LOGSTASH_CONF}" \
-    -v "elk_stack_pubcerts:${LOGSTASH_CERT_DIR}" \
-    -v "elk_stack_logstashdata02:${LOGSTASH_DIR}/data" \
-    -v "$(pwd)/logstash_ingest_data:${LOGSTASH_DIR}/ingest_data" \
+docker run --rm --name "logstash02" --network "${NETWORK}" \
+    -v "${PUBCERTS_VOLUME}:${LOGSTASH_CERT_DIR}" \
+    -v "${LOGSTASHDATA_VOLUME}:${LOGSTASH_DIR}/data" \
+    -v "$(pwd)/logstash-ingest-data:${LOGSTASH_DIR}/ingest-data" \
+    -v "$(pwd)/logstash-pipelines:${LOGSTASH_PIPELINE_DIR}" \
+    -v "$(pwd)/logstash-pipelines.yml:${LOGSTASH_CONF_DIR}/pipelines.yml" \
     -e ELASTIC_HOSTS="https://es01:9200" \
     -e ELASTIC_USER="elastic" \
     -e ELASTIC_PASSWORD="${ELASTIC_PASSWORD}" \
